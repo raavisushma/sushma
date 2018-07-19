d1d5c48638dbe26445cd214b51a141fd07a526b1
raavisushma <41369225+raavisushma@users.noreply.github.com>
2018-07-19 06:32:38 +0000

    task

---
+++ Task5.java
@@ -0,0 +1,23 @@
+package sushma.java;
+
+public class Task5 {
+
+public static void main(String[ ]args){
+int[]a={1,2,3,2,1};
+int length=a.length;
+for(int i=0;i<length;i++)
+{
+	for(int j=0;j<length;j++)
+	{
+		int lenth;
+		if(j==i||j==(length-1-i))
+		{
+			System.out.print(a[j]);
+		}
+		else{
+			System.out.print(" ");
+		}}
+	System.out.println( );
+		}
+	}
+}
